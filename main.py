7e32d91a17494e3ba82048ed8e8859dd7621d502
6399161357jayant <reoseki990@gmail.com>
2026-06-24 02:58:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -171,7 +171,6 @@ PROMOTE_RIGHTS = {
         can_invite_users=True,
         can_pin_messages=True,
         can_manage_video_chats=True,
-        can_edit_member_tags=True,
     ),
 
     3: dict(
@@ -182,7 +181,6 @@ PROMOTE_RIGHTS = {
         can_pin_messages=True,
         can_manage_video_chats=True,
         can_promote_members=True,
-        can_edit_member_tags=True,
         can_post_stories=True,
         can_edit_stories=True,
         can_delete_stories=True,
