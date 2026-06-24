bfd529e59b8b2930b637b583c5936c44360d6c42
6399161357jayant <reoseki990@gmail.com>
2026-06-24 12:52:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ try:
 except Exception:
     gemini_model = None
 # ── Constants ──────────────────────────────────────────────────────────────────
-OWNER_USERNAMES = ["light_speedy", "pyrox_speedy", "proxy_speedi"]
+OWNER_USERNAMES = ["light_speedy", "pyrox_speedy", "bbiry"]
 BOT_USERNAME = "nami_ibot"
 GROUP_LINK = "https://t.me/midnight_chatclub"
 NAMI_PHOTO_URL = "https://files.catbox.moe/vremhb.png"
